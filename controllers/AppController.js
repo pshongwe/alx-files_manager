796e04fb26312ec45510adc92eee07fdb9c2b07e
Darknught <hassino66@yahoo.com>
2024-08-02 02:38:55 -0700

    task 2

---
+++ controllers/AppController.js
@@ -0,0 +1,18 @@
+const { isDBAlive, getUserCount, getFileCount } = require('../utils/dbUtils');
+const { isRedisAlive } = require('../utils/redisUtils');
+
+class AppController {
+    static async getStatus(req, res) {
+        const dbStatus = await isDBAlive();
+        const redisStatus = await isRedisAlive();
+        res.status(200).send({ redis: redisStatus, db: dbStatus });
+    }
+
+    static async getStats(req, res) {
+        const userCount = await getUserCount();
+        const fileCount = await getFileCount();
+        res.status(200).send({ users: userCount, files: fileCount });
+    }
+}
+
+module.exports = AppController;
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Import routes
+const routes = require('./routes');
+
+// Use routes
+app.use('/', routes);
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
