f3b3c244700f3e827e475f5141bdd4a8b5955b50
Pibo Shongwe <piboman@yahoo.com>
2024-08-05 22:19:13 +0200

    db and redis

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,16 +1,17 @@
-const { isDBAlive, getUserCount, getFileCount } = require('../utils/db');
-const { isRedisAlive } = require('../utils/redis');
+const { isAlive: isDbAlive, nbUsers, nbFiles } = require('../utils/db');
+const { isAlive: isRedisAlive } = require('../utils/redis');
+
 
 class AppController {
   static async getStatus(req, res) {
-    const dbStatus = await isDBAlive();
+    const dbStatus = await isDbAlive();
     const redisStatus = await isRedisAlive();
     res.status(200).send({ redis: redisStatus, db: dbStatus });
   }
 
   static async getStats(req, res) {
-    const userCount = await getUserCount();
-    const fileCount = await getFileCount();
+    const userCount = await nbUsers();
+    const fileCount = await nbFiles();
     res.status(200).send({ users: userCount, files: fileCount });
   }
 }
